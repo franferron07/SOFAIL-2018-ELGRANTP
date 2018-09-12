f762a4e30fb02543d1643ab1ac2f2b01e7a1e80a
pacoDan <utnso@ubuntu-server>
2018-09-12 00:40:04 -0300

    configuracion pacoDanmdj.cfg

--- MDJ/src/MDJ.c
+++ MDJ/src/MDJ.c
@@ -8,12 +8,50 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <qcommons/socket.h> //Libreria Socket Cliente
-#include <qcommons/socketServer.h> //Libreria Socket Servidor
-
+#include "mdj.h"
 int main(void) {
 	puts("MDJ"); /* prints MDJ */
+
+	mdj_configuracion_inicial = malloc(sizeof(mdj_configuracion));
+
+	t_config *configuracion_cfg_temporal=cargar_en_memoria_cfg("mdj.cfg");
+
+	montar_configuracion(configuracion_cfg_temporal,mdj_configuracion_inicial);
+
+	config_destroy(configuracion_cfg_temporal);
+
+	mostrar_configuracion(mdj_configuracion_inicial);
+
+	config_destroy_mdj(mdj_configuracion_inicial);
+
 	return EXIT_SUCCESS;
+}
+t_config* cargar_en_memoria_cfg(char* dir){
+	t_config* aux = malloc(sizeof(mdj_configuracion));
+	aux=config_create(dir);
+	if(aux==NULL){
+		free(mdj_configuracion_inicial);
+		puts("no hay archivo mdj.cfg");
+	}
+	//config_create() carga en memoria el archivo .cfg ,en memoria se lo encuentra con &configuracion_temporal
+	return aux;
+}
+void montar_configuracion(t_config*  temporal,mdj_configuracion* configuracion){
+	//tomo las key para inicializar duplicando el string devuelvo para luego hacer los free
+	configuracion->mdj_puerto=string_duplicate(config_get_string_value(temporal,"PUERTO"));
+	configuracion->punto_de_montaje=string_duplicate(config_get_string_value(temporal,"PUNTO_MONTAJE"));
+	configuracion->retardo=config_get_int_value(temporal,"RETARDO");
+}
+
+void config_destroy_mdj(mdj_configuracion* mdj_configuracion_){
+	free(mdj_configuracion_->mdj_puerto);
+	free(mdj_configuracion_->punto_de_montaje);
+	free(mdj_configuracion_->retardo);
+}
+void mostrar_configuracion(mdj_configuracion* config){
+	puts("iniciando lectura ..");
+	printf("PUNTO_DE_MONTAJE = %s \n",config->punto_de_montaje);
+	printf("RETARDO = %d \n",config->retardo);
+	printf("PUERTO MDJ = %s \n",config->mdj_puerto);
+	puts("---fin lectura---");
 }
---
+++ MDJ/src/mdj.h
@@ -0,0 +1,33 @@
+/*
+ * mdj.h
+ *
+ *  Created on: 9 sep. 2018
+ *      Author: utnso
+ */
+#ifndef MDJ_H_
+#define MDJ_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h> //Commons config archivo
+#include <commons/string.h> //Commons string
+#include <qcommons/socket.h> //Libreria Socket Cliente
+#include <qcommons/socketServer.h> //Libreria Socket Servidor
+typedef struct{
+	char* mdj_puerto;
+	int retardo;
+	char* punto_de_montaje;
+}mdj_configuracion;
+
+//variables globales
+mdj_configuracion* mdj_configuracion_inicial=NULL;
+//int mdj_id;
+
+//funciones
+void liberar_memoria_de_configuracion(mdj_configuracion* configuracion_inicial);
+//freeshea el puntero de configuracion inicial
+void montar_configuracion(t_config* ,mdj_configuracion*);
+void mostrar_configuracion(mdj_configuracion* configuracion_inicial);
+t_config* cargar_en_memoria_cfg();
+
+
+#endif /* MDJ_H_ */
