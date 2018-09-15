54c497f1d33e095a161e81b9c7142c3899642baf
Luis Ore <luissk47@gmail.com>
2018-09-15 00:58:43 -0300

    primera lectura de configs y logs basico fm9

--- FM9/src/FM9.c
+++ FM9/src/FM9.c
@@ -8,13 +8,72 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <qcommons/socket.h> //Libreria Socket Cliente
-#include <qcommons/socketServer.h> //Libreria Socket Servidor
+#include "FM9.h"
+
 
 int main(void) {
 	puts("FM9"); /* prints FM9 */
-	puts("hola");
+
+	logger = log_create("FM9.log", "FM9",false, LOG_LEVEL_INFO);
+
+	log_info(logger, "INICIO FM9");
+
+	c_inicial = malloc(sizeof(config_inicial));
+
+	inicializador = config_create("fm9.cfg");
+
+	if (inicializador == NULL) {
+		free(c_inicial);
+		puts("No se encuentra archivo.");
+		log_error(logger, "No se encuentra archivo fm9.cfg");
+		exit(EXIT_FAILURE);
+	}
+
+	//leo archivo
+	leer_configuracion(inicializador, c_inicial);
+	log_info(logger, "Leido archivo fm9.cfg");
+
+	//muestro consola valor leido de archivo como prueba
+	prueba_leer_archivo_cfg(c_inicial);
+
+
+
+
+
+	/* libero memoria de inicializacion  */
+	config_destroy(inicializador);
+	/* libero loggger de logging */
+	log_destroy(logger);
+	/* libero struct config_inicial  */
+	liberarMemoriaConfig(c_inicial);
+
+
 	return EXIT_SUCCESS;
+}
+
+
+
+
+void leer_configuracion(t_config *inicializador , config_inicial *c_inicial ){
+	//tomo las key para inicializar duplicando el string devuelvo para luego hacer los free
+	c_inicial->puerto_escucha = string_duplicate(config_get_string_value(inicializador, "PUERTO"));
+	c_inicial->modo_ejecucion = string_duplicate(config_get_string_value(inicializador, "MODO"));
+	c_inicial->tam_memoria = config_get_int_value(inicializador, "TAMANIO");
+	c_inicial->tam_linea = config_get_int_value(inicializador, "MAX_LINEA");
+	c_inicial->tam_pagina = config_get_int_value(inicializador, "TAM_PAGINA");
+}
+
+void prueba_leer_archivo_cfg(config_inicial* c_inicial) {
+	puts("lectura de archivo correcta");
+	printf("PUERTO: %s \n",c_inicial->puerto_escucha);
+	printf("MODO: %s \n",c_inicial->modo_ejecucion);
+	printf("TAMANIO: %d \n",c_inicial->tam_memoria);
+	printf("MAX_LINEA: %d \n" ,c_inicial->tam_linea);
+	printf("TAM_PAGINA: %d \n",c_inicial->tam_pagina);
+}
+
+void liberarMemoriaConfig(config_inicial* c_inicial) {
+	free(c_inicial->puerto_escucha);
+	free(c_inicial->modo_ejecucion);
+	free(c_inicial);
 }
---
+++ FM9/src/FM9.h
@@ -0,0 +1,41 @@
+/*
+ * FM9.h
+ *
+ *  Created on: 15 sep. 2018
+ *      Author: cypher
+ */
+
+#ifndef FM9_H_
+#define FM9_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h> //Commons config archivo
+#include <commons/string.h> //Commons string
+#include <qcommons/socket.h> //Libreria Socket Cliente
+#include <qcommons/socketServer.h> //Libreria Socket Servidor
+
+typedef struct {
+	char *puerto_escucha;
+	char *modo_ejecucion;
+	int  tam_memoria;
+	int  tam_linea;
+	int  tam_pagina;
+} config_inicial;
+
+//Variables Globales
+config_inicial *c_inicial = NULL;
+t_config *inicializador=NULL;
+t_log *logger= NULL;
+
+
+//Funciones
+void liberarMemoriaConfig(config_inicial* c_inicial); //libera memoria de la configuracion inicial
+void leer_configuracion( t_config* , config_inicial*  ); //inicializa la configuracion inicial(lectura de archivo)
+void prueba_leer_archivo_cfg(config_inicial* c_inicial); //prueba leer archivo configuracion
+
+
+
+
+#endif /* FM9_H_ */
