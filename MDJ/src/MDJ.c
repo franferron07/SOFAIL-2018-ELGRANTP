87239f145646534f7a46e384d265826cb7437685
pacodan <pacdaolmed@est.frba.utn.edu.ar>
2018-09-16 00:37:37 -0300

    mdj escucha,falta probar

--- MDJ/src/MDJ.c
+++ MDJ/src/MDJ.c
@@ -8,31 +8,85 @@
  ============================================================================
  */
 
+
 #include "mdj.h"
+
+	int i;
+
 int main(void) {
-	puts("MDJ"); /* prints MDJ */
-	mdj_configuracion_inicial = malloc(sizeof(mdj_configuracion));
+
+	int socketCliente[MAX_CLIENTES];/* Descriptores de sockets con clientes */
+	fd_set descriptoresLectura;	/* Descriptores de interes para select() */
+	int numeroClientes = 0;			/* Número clientes conectados */
+	int maximo;							/* Número de descriptor más grande */
+	int buffer;							/* Buffer para leer de los socket */
+	int socketServidor;				/* Descriptor del socket servidor */
+
+		puts("MDJ"); /* prints MDJ */
+	mdj_setear_configuracion_default();
+	 Socket mdj_socket=crear_socket("127.0.0.1", mdj_configuracion_inicial->mdj_puerto);
+	 socketServidor = mdj_socket.socket;
+	 asociar_puerto(mdj_socket);
+	 escuchar(mdj_socket);
+	 while(1){
+
+		 compactaClaves(&socketServidor, &numeroClientes);
+
+		 FD_ZERO(&descriptoresLectura);
+
+		 FD_SET(socketServidor,&descriptoresLectura);
+		 for (i = 0; i < numeroClientes; i++) {
+			 FD_SET(socketCliente[i],&descriptoresLectura);
+					 ;
+			 maximo=dameMaximo(socketCliente, numeroClientes);
+		 }
+		 if (maximo<socketServidor)maximo=socketServidor;
+		 /* Espera indefinida hasta que alguno de los descriptores tenga algo
+		 			 * que decir: un nuevo cliente o un cliente ya conectado que envía un
+		 			 * mensaje */
+		 select (maximo + 1, &descriptoresLectura, NULL, NULL, NULL);
+
+		 /* Se comprueba si algún cliente ya conectado ha enviado algo */
+		 for (i = 0;  i < numeroClientes; i++) {
+			 if (FD_ISSET(socketCliente[i],&descriptoresLectura)) {/* Se lee lo enviado por el cliente y se escribe en pantalla */
+				if ((Lee_Socket (socketCliente[i], (char *)&buffer, sizeof(int)) > 0))
+					printf ("Cliente %d envía %d\n", i+1, buffer);
+				else{
+					/* Se indica que el cliente ha cerrado la conexión y se
+					 * marca con -1 el descriptor para que compactaClaves() lo
+					 * elimine */
+					printf ("Cliente %d ha cerrado la conexión\n", i+1);
+					socketCliente[i] = -1;
+				}
+				/* Se comprueba si algún cliente nuevo desea conectarse y se le
+							 * admite */
+				if (FD_ISSET (socketServidor, &descriptoresLectura))
+					nuevoCliente (socketServidor, socketCliente, &numeroClientes);
+				}
+			}
+		}
+	 }
+
+void mdj_setear_configuracion_default(){
+	mdj_configuracion_inicial=malloc(sizeof(mdj_configuracion));
 	t_config *configuracion_cfg_temporal=cargar_en_memoria_cfg("mdj.cfg");
-
 	montar_configuracion(configuracion_cfg_temporal,mdj_configuracion_inicial);
-
 	config_destroy(configuracion_cfg_temporal);
-	mostrar_configuracion(mdj_configuracion_inicial);
-
-	config_destroy_mdj(mdj_configuracion_inicial);
-
-	return EXIT_SUCCESS;
 }
+
+
+
 t_config* cargar_en_memoria_cfg(char* dir){
 	t_config* aux = malloc(sizeof(mdj_configuracion));
 	aux=config_create(dir);
 	if(aux==NULL){
 		free(mdj_configuracion_inicial);
-		puts("no hay archivo mdj.cfg");
+		puts("Error");
 	}
 	//config_create() carga en memoria el archivo .cfg ,en memoria se lo encuentra con &configuracion_temporal
 	return aux;
 }
+
 void montar_configuracion(t_config*  temporal,mdj_configuracion* configuracion){
 	//tomo las key para inicializar duplicando el string devuelvo para luego hacer los free
 	configuracion->mdj_puerto=string_duplicate(config_get_string_value(temporal,"PUERTO"));
--- MDJ/src/mdj.h
+++ MDJ/src/mdj.h
@@ -12,11 +12,18 @@
 #include <commons/string.h> //Commons string
 #include <qcommons/socket.h> //Libreria Socket Cliente
 #include <qcommons/socketServer.h> //Libreria Socket Servidor
+//----------------------------
 typedef struct{
 	char* mdj_puerto;
 	int retardo;
 	char* punto_de_montaje;
 }mdj_configuracion;
+//----------------------------
+
+
+
+
+
 
 //variables globales
 mdj_configuracion* mdj_configuracion_inicial=NULL;
@@ -27,7 +34,8 @@ void liberar_memoria_de_configuracion(mdj_configuracion* configuracion_inicial);
 //freeshea el puntero de configuracion inicial
 void montar_configuracion(t_config* ,mdj_configuracion*);
 void mostrar_configuracion(mdj_configuracion* configuracion_inicial);
-t_config* cargar_en_memoria_cfg();
+t_config* cargar_en_memoria_cfg(char*);
+void config_destroy_mdj(mdj_configuracion* );
 
 
 #endif /* MDJ_H_ */
