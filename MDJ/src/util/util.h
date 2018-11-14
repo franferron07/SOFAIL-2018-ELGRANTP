79c7d603517d8666622cd4023ed2d53812100ab2
Jhon Daniel <pacdaolmed@est.frba.utn.edu.ar>
2018-11-14 19:33:12 -0300

    Add files via upload

---
+++ MDJ/src/util/util.h
@@ -0,0 +1,16 @@
+/*
+ * util.h
+ *
+ *  Created on: 13 nov. 2018
+ *      Author: jhondaniel
+ */
+
+#ifndef UTIL_UTIL_H_
+#define UTIL_UTIL_H_
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+char* recortarPrimerosCaracteres(char* s, int primerosCaracteres);//ok y  malloquea automaticamente
+
+
+#endif /* UTIL_UTIL_H_ */
